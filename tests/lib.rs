d2a863c16fda884038bdc8095554d599c990c1a5
ndr <ndr@qef.io>
2016-02-18 18:24:33 +0000

    Expunge test for README contents
    To be replaced with testable examples.

--- tests/lib.rs
+++ tests/lib.rs
@@ -52,26 +52,3 @@ fn basics() {
     let s1: String = h1.punctuate().collect();
     assert_eq!(s1, "hy\u{ad}phen\u{ad}ation".to_owned());
 }
-
-#[test]
-fn readme() {
-    use hyphenation::{Hyphenation, Standard};
-    use hyphenation::Language::{English_US};
-
-    // Load hyphenation data for American English.
-    let english_us = hyphenation::load(English_US).unwrap();
-
-    // An iterator that breaks a word according to standard hyphenation practices.
-    let h: Standard = "hyphenation".hyphenate(&english_us);
-                    // hy-phen-ation
-
-    // Collect the lazy hyphenator `h` into substring slices over the original string.
-    let v: Vec<&str> = h.collect();
-    assert_eq!(v, vec!["hy", "phen", "ation"]);
-
-    // Mark hyphenation opportunities with soft hyphens,
-    // and render the result to a new String.
-    let h1 = "anfractuous".hyphenate(&english_us);
-    let s: String = h1.punctuate().collect();
-    assert_eq!(s, "an\u{ad}frac\u{ad}tu\u{ad}ous".to_owned());
-}
